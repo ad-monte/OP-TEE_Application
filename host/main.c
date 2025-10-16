8c67e8980d785fb6aa28c81dadfb5d2d6d3ed9e8
Alfonso sebastian s337020 <s337020@studenti.polito.it>
2025-10-16 11:36:41 +0200

    add functions into the main

--- host/main.c
+++ host/main.c
@@ -5,6 +5,76 @@
 #include <tee_client_api.h>
 #include <my_ta.h> 
 
+struct test_ctx {
+	TEEC_Context ctx;
+	TEEC_Session sess;
+};
+
+void prepare_tee_session(struct test_ctx *ctx){
+
+	TEEC_Result res;
+	TEEC_UUID uuid = MY_TA_UUID;
+	uint32_t err_origin;
+
+	res = TEEC_InitializeContext(NULL, &ctx->ctx);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
+	res = TEEC_OpenSession(&ctx->ctx, &ctx->sess, &uuid, TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",res, err_origin);
+
+}
+
+void terminate_tee_session(struct test_ctx *ctx){
+	TEEC_CloseSession(&ctx->sess);
+	TEEC_FinalizeContext(&ctx->ctx);
+}
+
+void get_secret(uint8_t* secret_get, uint32_t size,  struct test_ctx *ctx_sess){
+
+	TEEC_Operation op;
+	TEEC_Result res;
+	uint32_t err_origin;
+
+	memset(secret_get, 0,size);
+	memset(&op, 0, sizeof(op));
+
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_OUTPUT, TEEC_VALUE_INPUT,
+					 TEEC_NONE, TEEC_NONE);
+
+	op.params[0].tmpref.buffer = secret_get;
+	op.params[0].tmpref.size = size;
+	op.params[1].value.a = 1;
+
+	res = TEEC_InvokeCommand(&ctx_sess->sess, CMD_SECRET_MANAGMENT_GET, &op, &err_origin);
+
+}
+
+void store_secret(uint8_t* secret, uint32_t size, struct test_ctx *ctx_sess){
+
+	TEEC_Operation op;
+	TEEC_Result res;
+	uint32_t err_origin;
+
+	memset(&op, 0, size);
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT, TEEC_VALUE_INPUT,
+					 TEEC_NONE, TEEC_NONE);
+
+	op.params[0].tmpref.buffer = secret;	
+	op.params[0].tmpref.size = size;
+	op.params[1].value.a = 1;
+
+	res = TEEC_InvokeCommand(&ctx_sess->sess, CMD_SECRET_MANAGMENT_STR, &op, &err_origin);
+}
+
+void print_buffer(uint8_t* buffer, uint32_t size, const char* msg){
+	printf("%s",msg);
+	for(int i=0; i < size; i++){
+		printf("%c",buffer[i]);
+	}
+	printf("\n");
+}
+
 int main(void)
 {
 	TEEC_Result res;
@@ -14,65 +84,34 @@ int main(void)
 	TEEC_UUID uuid = MY_TA_UUID;
 	uint32_t err_origin;
 
-	/* Initialize a context connecting us to the TEE */
-	res = TEEC_InitializeContext(NULL, &ctx);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
-
-	/*
-	 * Open a session to the "hello world" TA, the TA will print "hello
-	 * world!" in the log when the session is created.
-	 */
-	res = TEEC_OpenSession(&ctx, &sess, &uuid,
-			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
-			res, err_origin);
-
-	/*
-	 * Execute a function in the TA by invoking it, in this case
-	 * we're incrementing a number.
-	 *
-	 * The value of command ID part and how the parameters are
-	 * interpreted is part of the interface provided by the TA.
-	 */
-
-	/* Clear the TEEC_Operation struct */
-	memset(&op, 0, sizeof(op));
-
-	/*
-	 * Prepare the argument. Pass a value in the first parameter,
-	 * the remaining three parameters are unused.
-	 */
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
-					 TEEC_NONE, TEEC_NONE);
-
-	op.params[0].value.a = 42;
-
-	/*
-	 * TA_HELLO_WORLD_CMD_INC_VALUE is the actual function in the TA to be
-	 * called.
-	 */
-	printf("Invoking TA to increment %d\n", op.params[0].value.a);
+	struct test_ctx ctx_sess1;
 	
-	res = TEEC_InvokeCommand(&sess, MY_TA_CMD_INC_VALUE, &op, &err_origin);
+	ctx_sess1.ctx = ctx;
+	ctx_sess1.sess = sess;
+
+	prepare_tee_session(&ctx_sess1);
+
+
+
+	// Test 0: Retrieve secret uninitialized (information disclosure)
+	uint8_t secret_get[64] = {0};
+	memset(secret_get, 0,sizeof(secret_get)); 
+
+	get_secret(secret_get, sizeof(secret_get) ,&ctx_sess1);  // Get secret using sess 1
+
+	print_buffer(secret_get, sizeof(secret_get), "Secret uninitizalized: ");
+
+	// Test1 : store secret
+	uint8_t secret[32] = {0};  
+	memset(secret, 'B', sizeof(secret));
 	
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
-			res, err_origin);
-	printf("TA incremented value to %d\n", op.params[0].value.a);
+	print_buffer(secret, sizeof(secret), "Store secret: ");
 
-	/*
-	 * We're done with the TA, close the session and
-	 * destroy the context.
-	 *
-	 * The TA will print "Goodbye!" in the log when the
-	 * session is closed.
-	 */
+	store_secret(secret, sizeof(secret), &ctx_sess1); // Store secret using sess 1
 
-	TEEC_CloseSession(&sess);
 
-	TEEC_FinalizeContext(&ctx);
+
+	terminate_tee_session(&ctx_sess1);
 
 	return 0;
 }
